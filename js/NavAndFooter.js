c812c11ca405cc451966d08541d9e366fdb5443a
a298034a <a298034aa298034a@gmail.com>
2024-09-11 23:09:32 +0800

    導覽列RWD製作中

--- js/NavAndFooter.js
+++ js/NavAndFooter.js
@@ -20,12 +20,17 @@ function Nav() {
     return (
         <header id="topbar" style={{
             borderBottom: isScrolled ? "5px solid #304173" : "5px solid transparent",
-            boxShadow: isScrolled ? "0px 1px 2px 0px rgba(0, 0, 0, 25%)" : "none"            
+            boxShadow: isScrolled ? "0px 1px 2px 0px rgba(0, 0, 0, 25%)" : "none"
         }}>
             <a className="logo" href="./index.html">
                 <img src="./images/logo-full-b.png" alt="回首頁" />
             </a>
-            <div className="menu">
+            <div className="hambergur">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <nav className="menu">
                 <ul>
                     <li><a href="./property-management.html">包租代管</a></li>
                     <li><a href="./furnish.html">裝修服務</a></li>
@@ -33,7 +38,7 @@ function Nav() {
                     <li><a href="./login.html">登入註冊</a></li>
                     <li><a href="./consult.html">免費諮詢</a></li>
                 </ul>
-            </div>
+            </nav>
         </header>
     )
 }
