337ffd2a4132933f1e6d8f01d13e1d2a18e7e9c3
a298034a <a298034aa298034a@gmail.com>
2024-09-12 23:13:22 +0800

    導覽列RWD

--- js/NavAndFooter.js
+++ js/NavAndFooter.js
@@ -1,6 +1,19 @@
 function Nav() {
     const [isScrolled, setIsScrolled] = useState(false);
 
+    const [menuActive, setMenuActive] = useState(false);
+    const [reverseAnim, setReverseAnim] = useState(false);
+
+    const handleMenuClick = () => {
+        setMenuActive(!menuActive);
+        setReverseAnim(false); // 每次點擊漢堡按鈕時，移除 reverse_anim
+    };
+
+    const handleCloseClick = () => {
+        setReverseAnim(true); // 點擊關閉按鈕時啟動 reverse_anim
+        setMenuActive(false);
+    };
+
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -25,7 +38,7 @@ function Nav() {
             <a className="logo" href="./index.html">
                 <img src="./images/logo-full-b.png" alt="回首頁" />
             </a>
-            <div className="hambergur">
+            <div className="hambergur" onClick={handleMenuClick}>
                 <div></div>
                 <div></div>
                 <div></div>
@@ -38,6 +51,18 @@ function Nav() {
                     <li><a href="./login.html">登入註冊</a></li>
                     <li><a href="./consult.html">免費諮詢</a></li>
                 </ul>
+            </nav>
+            <nav className={`pad-menu ${menuActive ? 'active' : ''} ${reverseAnim ? 'reverse_anim' : ''}`}>
+                <ul>
+                    <li><a href="./login.html">登入註冊</a></li>
+                    <li><a href="./consult.html">免費諮詢</a></li>
+                    <li><a href="./property-management.html">包租代管</a></li>
+                    <li><a href="./furnish.html">裝修服務</a></li>
+                    <li><a href="./system-describe.html">系統介紹</a></li>
+                </ul>
+                <div class="close" onClick={handleCloseClick}>
+                    <div></div>
+                </div>
             </nav>
         </header>
     )
